b560e2b5acb44a947897ed67e07bf2fb47b7285a
Tianhao-Gu <tgu@anl.gov>
2020-02-17 14:41:32 -0600

    using local autodocker tools

--- lib/CompoundSetUtils/CompoundSetUtilsImpl.py
+++ lib/CompoundSetUtils/CompoundSetUtilsImpl.py
@@ -11,7 +11,6 @@ import CompoundSetUtils.compound_parsing as parse
 import CompoundSetUtils.zinc_db_util as zinc_db_util
 from installed_clients.DataFileUtilClient import DataFileUtil
 from installed_clients.KBaseReportClient import KBaseReport
-from installed_clients.AD_VINAClient import AD_VINA
 #END_HEADER
 
 
@@ -130,6 +129,36 @@ Contains tools for import & export of compound sets
 
         return packed_mol2_files_path, comp_id_mol2_file_name_map
 
+    def _mol2_to_pdbqt(self, mol2_file_path, compound_id):
+
+        logging.info('start converting mol2 to pdbqt')
+
+        autodocker_util_dir = '/usr/local/lib/mgltools_x86_64Linux2_1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24'
+
+        pdbqt_temp_dir = "{}/{}".format(self.scratch, uuid.uuid4())
+        os.mkdir(pdbqt_temp_dir)
+
+        pdbqt_file_path = os.path.join(pdbqt_temp_dir, compound_id + '.pdbqt')
+
+        prepare_ligand4_filepath = os.path.join(autodocker_util_dir, 'prepare_ligand4.py')
+
+        command = ['python2.5', prepare_ligand4_filepath, '-l', mol2_file_path, '-o', pdbqt_file_path]
+
+        process = Popen(command, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+
+        logging.info('completed prepare_ligand4\nstdout:{}\nstderr:{}\n'.format(stdout, stderr))
+
+        if not os.path.exists(pdbqt_file_path):
+            pdbqt_file_path = ''
+            raise ValueError('failed to convert mol2 to pdbqt format')
+        else:
+            file_size = os.path.getsize(pdbqt_file_path)
+            if file_size == 0:
+                raise ValueError('generated empty pdbqt file')
+
+        return pdbqt_file_path
+
     def _covert_mol2_files_to_pdbqt(self, ref):
         compoundset_obj = self.dfu.get_objects(
             {'object_refs': [ref]}
@@ -153,7 +182,7 @@ Contains tools for import & export of compound sets
                                             {'handle_id': mol2_handle_ref,
                                              'file_path': mol2_temp_dir}).get('file_path')
 
-                pdbqt_file_path = self.ad_vina.mol2_to_pdbqt(mol2_file_path, compound['id'])
+                pdbqt_file_path = self._mol2_to_pdbqt(mol2_file_path, compound['id'])
                 if pdbqt_file_path:
                     pdbqt_files.append(pdbqt_file_path)
                     comp_id_pdbqt_file_name_map[compound['id']] = os.path.basename(pdbqt_file_path)
@@ -194,7 +223,6 @@ Contains tools for import & export of compound sets
         self.scratch = config['scratch']
         self.callback_url = os.environ['SDK_CALLBACK_URL']
         self.dfu = DataFileUtil(self.callback_url)
-        self.ad_vina = AD_VINA(self.callback_url)
         logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
                             level=logging.INFO)
         #END_CONSTRUCTOR
